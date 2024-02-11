06756c2b087004e83d905f6a62b180887cf4a6df
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2024-02-11 23:42:21 +0100

    cleanups

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import Script from "next/script";
 import { AuthLayout } from '@app/layouts';
-import SignUpPage from '@app/components/templates/signup';
+import SignUpPage from '@app/components/templates/SignUp';
 
 function Signup(): JSX.Element {
     return (
@@ -10,7 +10,7 @@ function Signup(): JSX.Element {
             <Head>
                 <title>Register | Plusity</title>
             </Head>
-            <SignUpPage/>
+            <SignUpPage />
              <Script
             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBfZGKX2eI8YWey2xWEct0hXqZot0cWc6I&libraries=places"
             />
