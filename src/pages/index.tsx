92329abe58e8f3b2e7909617ae499fc9805b0969
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2024-02-12 11:15:38 +0100

    cleanups

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ function App({ productCategoryData, homeCategoryProductsData } : { productCatego
             <Head>
                 <title>Home - Welcome to Plusity</title>
             </Head>
-            { productCategoryData && homeCategoryProductsData && <HomePage productCategories={productCategoryData} categoryProducts={homeCategoryProductsData} /> }
+            <HomePage productCategories={productCategoryData} categoryProducts={homeCategoryProductsData} />
         </>
     );
 }
