bc1ba6d2162277cac3a37bb1db15b2892c7a1375
supremozee <supremozee14@gmail.com>
2024-02-13 12:17:19 +0100

    rebasing and conflict fixed

--- src/app/components/templates/HomePage/index.tsx
+++ src/app/components/templates/HomePage/index.tsx
@@ -4,6 +4,7 @@ import {HomeCategoryProducts } from '@app/components/organisms/HomeCategoryProdu
 import { ProductCategory } from '@app/components/organisms/ProductCategory';
 import { ShoppingHeaderImage } from '@app/components/organisms/shop-image';
 import { HomeCategoryProductsData, ProductCategoryData, TFeaturedBannerData } from '@app/types';
+import Loader from '@app/components/atoms/Loader';
 
 
 const HomeContainer = styled.div`
@@ -11,16 +12,24 @@ const HomeContainer = styled.div`
     margin: 0 auto;
     padding: 0;
 `
-const HomePage =  ({ productCategories, categoryProducts, featuredBanners } : { productCategories: ProductCategoryData, categoryProducts : HomeCategoryProductsData, featuredBanners : TFeaturedBannerData }) => {
-    return (
-        <>
-        { featuredBanners && <ShoppingHeaderImage initialFeaturedBanners={featuredBanners} /> }
+
+const HomePage =  ({ productCategories, categoryProducts, featuredBanners } : { productCategories: ProductCategoryData, categoryProducts : HomeCategoryProductsData ,  featuredBanners : TFeaturedBannerData}) => {
+    let renderedContent;
+    try {
+        renderedContent = (
+            <>
+            { featuredBanners && <ShoppingHeaderImage initialFeaturedBanners={featuredBanners} /> }
         <HomeContainer>
             { productCategories && <ProductCategory initialProductCategories={productCategories}/>}
             { categoryProducts && <HomeCategoryProducts initialCategoryProducts={categoryProducts}/>}
         </HomeContainer>
-        </>         
-    );
+            </>
+        );
+    } catch (error:any) {
+        console.error('Error occurred in HomePage:', error);
+        renderedContent = <Loader color='green' height={300}/>
+    }
+    return renderedContent;
 };
 
 
